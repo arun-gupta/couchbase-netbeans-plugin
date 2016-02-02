7d552c2c735d28c07051c0499bd8e552ecf33687
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-02-02 13:01:24 +0100

    Separating queries from GUI container.

--- src/main/java/org/netbeans/modules/couchbase/bucket/QueryContainerTopComponent.java
+++ src/main/java/org/netbeans/modules/couchbase/bucket/QueryContainerTopComponent.java
@@ -4,14 +4,9 @@ import com.couchbase.client.java.Bucket;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Set;
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
 import net.miginfocom.swing.MigLayout;
-import org.openide.util.NbPreferences;
+import org.netbeans.modules.couchbase.bucket.queries.NoOfDisplayedDocumentsQuery;
+import org.netbeans.modules.couchbase.bucket.queries.WhereDisplayedDocumentsQuery;
 import org.openide.util.lookup.Lookups;
 import org.openide.windows.Mode;
 import org.openide.windows.TopComponent;
@@ -33,48 +28,9 @@ public class QueryContainerTopComponent extends ObjectTopComponent implements Ac
     public QueryContainerTopComponent(final Bucket bucket) {
         this.bucket = bucket;
         setLayout(new MigLayout(""));
-        noOfDisplayedDocumentsQuery(bucket);
-        rangeDisplayedDocumentsQuery(bucket);
+        add(new NoOfDisplayedDocumentsQuery(bucket), "wrap");
+        add(new WhereDisplayedDocumentsQuery(bucket), "wrap");
         associateLookup(Lookups.singleton(bucket));
-    }
-
-    private void noOfDisplayedDocumentsQuery(final Bucket bucket1) {
-        JPanel panel = new JPanel(new MigLayout());
-        panel.setBorder(new TitledBorder("Basic Query"));
-        JLabel label = new JLabel("No. of displayed documents:");
-        final JTextField noOfDisplayedDocuments = new JTextField(50);
-        noOfDisplayedDocuments.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                NbPreferences.forModule(BucketNode.class).put(bucket1.name() + "-numberOfRows", noOfDisplayedDocuments.getText());
-            }
-        });
-        panel.add(label);
-        panel.add(noOfDisplayedDocuments);
-        add(panel, "wrap");
-    }
-    
-    private void rangeDisplayedDocumentsQuery(final Bucket bucket) {
-        JPanel panel = new JPanel(new MigLayout());
-        panel.setBorder(new TitledBorder("Complex Query"));
-        JLabel fromLabel = new JLabel("Where:");
-        final JTextField whereField = new JTextField(20);
-        JLabel toLabel = new JLabel("Equals:");
-        final JTextField equalsField = new JTextField(20);
-        JButton okButton = new JButton("OK");
-        okButton.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                NbPreferences.forModule(BucketNode.class).put(bucket.name() + "-where", whereField.getText());
-                NbPreferences.forModule(BucketNode.class).put(bucket.name() + "-equals", equalsField.getText());
-            }
-        });
-        panel.add(fromLabel);
-        panel.add(whereField);
-        panel.add(toLabel);
-        panel.add(equalsField);
-        panel.add(okButton);
-        add(panel, "wrap");
     }
 
     @Override
