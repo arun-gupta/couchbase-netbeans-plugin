f5c98131f41d0d60abc843c967ee67a5a8c61e2f
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-02-02 17:24:30 +0100

    Separating query components into separate topcomponents.

--- src/main/java/org/netbeans/modules/couchbase/bucket/queries/NoOfDisplayedDocumentsQuery.java
+++ src/main/java/org/netbeans/modules/couchbase/bucket/queries/NoOfDisplayedDocumentsQuery.java
@@ -2,17 +2,25 @@ package org.netbeans.modules.couchbase.bucket.queries;
 
 import com.couchbase.client.java.Bucket;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
 import javax.swing.AbstractAction;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 import org.netbeans.modules.couchbase.bucket.BucketNode;
 import org.openide.util.NbPreferences;
+import org.openide.util.lookup.Lookups;
+import org.openide.windows.Mode;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
 
-public class NoOfDisplayedDocumentsQuery extends JPanel {
+public class NoOfDisplayedDocumentsQuery extends TopComponent implements ActionListener {
+
+    private final Bucket bucket;
 
     public NoOfDisplayedDocumentsQuery(final Bucket bucket) {
+        this.bucket = bucket;
         setLayout(new MigLayout());
         JLabel label = new JLabel("No. of displayed documents:");
         final JTextField noOfDisplayedDocuments = new JTextField(50);
@@ -24,6 +32,31 @@ public class NoOfDisplayedDocumentsQuery extends JPanel {
         });
         add(label);
         add(noOfDisplayedDocuments);
+        associateLookup(Lookups.singleton(bucket));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setDisplayName(bucket.name());
+        Mode navigatorMode = WindowManager.getDefault().findMode("navigator");
+        navigatorMode.dockInto(this);
+        TopComponent tc = findTopComponent(bucket);
+        if (tc == null) {
+            this.open();
+            this.requestActive();
+        } else {
+            tc.requestActive();
+        }
+    }
+
+    private TopComponent findTopComponent(Bucket bucket) {
+        Set<TopComponent> openTopComponents = WindowManager.getDefault().getRegistry().getOpened();
+        for (TopComponent tc : openTopComponents) {
+            if (tc.getLookup().lookup(Bucket.class) == bucket && tc.getClass().getSimpleName().equals("NoOfDisplayedDocumentsQuery")) {
+                return tc;
+            }
+        }
+        return null;
     }
 
 }
--- src/main/java/org/netbeans/modules/couchbase/bucket/queries/WhereDisplayedDocumentsQuery.java
+++ src/main/java/org/netbeans/modules/couchbase/bucket/queries/WhereDisplayedDocumentsQuery.java
@@ -2,6 +2,8 @@ package org.netbeans.modules.couchbase.bucket.queries;
 
 import com.couchbase.client.java.Bucket;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Set;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -11,10 +13,17 @@ import javax.swing.border.TitledBorder;
 import net.miginfocom.swing.MigLayout;
 import org.netbeans.modules.couchbase.bucket.BucketNode;
 import org.openide.util.NbPreferences;
+import org.openide.util.lookup.Lookups;
+import org.openide.windows.Mode;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
 
-public class WhereDisplayedDocumentsQuery extends JPanel {
+public class WhereDisplayedDocumentsQuery extends TopComponent implements ActionListener {
+
+    private final Bucket bucket;
 
     public WhereDisplayedDocumentsQuery(final Bucket bucket) {
+        this.bucket = bucket;
         setLayout(new MigLayout());
         JPanel panel = new JPanel(new MigLayout());
         panel.setBorder(new TitledBorder("Complex Query"));
@@ -35,6 +44,31 @@ public class WhereDisplayedDocumentsQuery extends JPanel {
         add(toLabel);
         add(equalsField);
         add(okButton);
+        associateLookup(Lookups.singleton(bucket));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        setDisplayName(bucket.name());
+        Mode navigatorMode = WindowManager.getDefault().findMode("navigator");
+        navigatorMode.dockInto(this);
+        TopComponent tc = findTopComponent(bucket);
+        if (tc == null) {
+            this.open();
+            this.requestActive();
+        } else {
+            tc.requestActive();
+        }
+    }
+
+    private TopComponent findTopComponent(Bucket bucket) {
+        Set<TopComponent> openTopComponents = WindowManager.getDefault().getRegistry().getOpened();
+        for (TopComponent tc : openTopComponents) {
+            if (tc.getLookup().lookup(Bucket.class) == bucket && tc.getClass().getSimpleName().equals("WhereDisplayedDocumentsQuery")) {
+                return tc;
+            }
+        }
+        return null;
     }
 
 }
