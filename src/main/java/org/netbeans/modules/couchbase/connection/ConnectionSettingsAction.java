7ea3d248264acc48ee7b683934a6c22a5416e97a
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-03-21 01:19:53 +0100

    Start of settings menu on Connection node.

--- src/main/java/org/netbeans/modules/couchbase/connection/ConnectionSettingsAction.java
+++ src/main/java/org/netbeans/modules/couchbase/connection/ConnectionSettingsAction.java
@@ -1,19 +1,12 @@
 package org.netbeans.modules.couchbase.connection;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.prefs.Preferences;
 import javax.swing.AbstractAction;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import org.openide.DialogDisplayer;
-import org.openide.NotifyDescriptor;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.NbBundle.Messages;
-import org.openide.util.NbPreferences;
 import org.openide.util.actions.Presenter;
-import org.openide.util.actions.SystemAction;
 
 @ActionID(
         category = "Connection",
